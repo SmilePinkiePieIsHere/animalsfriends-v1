618b272f652f3d32020ad091a63845aea6353453
SmilePinkiePieIsHere <pevanzi@abv.bg>
2021-04-10 23:20:25 +0300

    fix register validation

--- src/components/General/UserState/Register.js
+++ src/components/General/UserState/Register.js
@@ -11,64 +11,75 @@ function Register({
     history
 }) {
     const [cookies, setCookie] = useCookies();
+    const [isValid, setIsValid] = useState(false);
     const [state, setState] = useState({
-            alertShow: false,
-            alertTitle: '',
-            alertText: '',            
-            alertClass: ''
-        }
-    );    
+        alertShow: false,
+        alertTitle: '',
+        alertText: '',
+        alertClass: ''
+    }
+    );
 
-    const alertDetails = (shouldShow, message, classAlert) => { 
+    const alertDetails = (shouldShow, message, classAlert) => {
         setState({
             alertShow: shouldShow,
-            alertText: message,            
+            alertText: message,
             alertClass: classAlert
         });
     }
 
     const submitHandler = (e) => {
         e.preventDefault();
-        
-        let form = e.target;       
-        let userTemp = {
-            username: form.username.value,
-            password: form.password.value,
-            password2: form.passwordConfirmation.value
-        }
-       
-        if (userTemp.password === userTemp.password2) {
-            postData(endpoints.userRegister, userTemp, function (data) {               
-                if (!data.error_description) {
-                    const expires_in = new Date(new Date().getTime() + (data.expires_in * 1000));
-                    setCookie('access_token', data.access_token, { expires: expires_in });
-                    setCookie('refresh_token', data.refresh_token);
-                    setCookie('username', userTemp.username);
-    
-                    alertDetails(true, "Успешно се регистрирахте!", "success");
-                    setTimeout(() => {
-                        history.goBack();
-                    }, 3000);
-                }
-                else {
-                    alert(data.error_description);
-                }               
 
-            }, function (error) {
-                alertDetails(true, "Грешка от страна на сървъра при регистрация!", "danger");
+        if (e.currentTarget.checkValidity() === false) {
+            e.stopPropagation();
+        } 
+        else {
+            let form = e.target;
+            let userTemp = {
+                username: form.username.value,
+                password: form.password.value,
+                password2: form.passwordConfirmation.value
+            }
+
+            if (userTemp.password === userTemp.password2) {
+                postData(endpoints.userRegister, userTemp, function (data) {
+                    if (!data.error_description) {
+                        const expires_in = new Date(new Date().getTime() + (data.expires_in * 1000));
+                        setCookie('access_token', data.access_token, { expires: expires_in });
+                        setCookie('refresh_token', data.refresh_token);
+                        setCookie('username', userTemp.username);
+
+                        alertDetails(true, "Успешно се регистрирахте!", "success");
+                        setTimeout(() => {
+                            history.goBack();
+                        }, 3000);
+                    }
+                    else {
+                        alert(data.error_description);
+                    }
+
+                }, function (error) {
+                    alertDetails(true, "Грешка от страна на сървъра при регистрация!", "danger");
+
+                    setTimeout(() => {
+                        alertDetails(false, "Паролите не съвпадат!", "danger");
+                    }, 3000);
+                })
+            }
+            else {
+                form.password.value = "";
+                form.passwordConfirmation.value = "";
+
+                alertDetails(true, "Паролите не съвпадат!", "danger");
 
                 setTimeout(() => {
                     alertDetails(false, "Паролите не съвпадат!", "danger");
                 }, 3000);
-            })
+            }
         }
-        else {
-            alertDetails(true, "Паролите не съвпадат!", "danger");
 
-            setTimeout(() => {
-                alertDetails(false, "Паролите не съвпадат!", "danger");
-            }, 3000);
-        }
+        setIsValid(true);
     }
 
     return (
@@ -78,11 +89,10 @@ function Register({
                 <Col xs={6}>
                     {
                         state.alertShow
-                        ? (<AlertNotification text={state.alertText} heading={state.alertTitle} variant={state.alertClass} show={state.alertShow} />)
-                        : null       
+                            ? (<AlertNotification text={state.alertText} heading={state.alertTitle} variant={state.alertClass} show={state.alertShow} />)
+                            : null
                     }
-                    <Form className="form-view" onSubmit={submitHandler}>
-                        {/* noValidate validated={this.state.isValid}  */}
+                    <Form className="form-view" noValidate validated={isValid} onSubmit={submitHandler}>
                         <Form.Group>
                             <Form.Control required type="text" name="username" placeholder="Потребителско име" />
                             <Form.Control.Feedback type="invalid">Моля, въведете потребителско име.</Form.Control.Feedback>

33aba87d85189752d0a961f830209bad8e5d007c
SmilePinkiePieIsHere <pevanzi@abv.bg>
2021-04-10 15:16:14 +0300

    refactor services

--- src/services/services.js
+++ src/services/services.js
@@ -1,97 +1,51 @@
 import getAccessToken from "./auth.js";
 
-const accessToken = getAccessToken();
+async function baseFetch(urlRequest, data = {}, onSuccess, onFailure, methodRequest, failureMsg, hasBody = true, hasAuth = false) {
+    let headers =  {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'          
+    };
 
-// async function baseFetch(url, method, headers) {
-//     return await fetch(url, {
-//         method: 'POST',
-//         mode: 'cors', // no-cors, *cors, same-origin
-//         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//         credentials: 'same-origin', // include, *same-origin, omit
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Accept': 'application/json'          
-//         },  
-//         redirect: 'follow', // manual, *follow, error
-//         referrerPolicy: 'no-referrer', // no-referrer, *client      
-//         body: JSON.stringify(data)
-//     })
-//     .then(res => res.json())
-//     .then((data) => {       
-//         onSuccess(data);
-//     })
-//     .catch(error => {  
-//         console.log(error); 
-//         onFailure('Сървърна грешка при оторизацията!');
-//     });
-// }
+    if (hasAuth) {
+        let accessToken = await getAccessToken();
+        headers['Authorization'] = 'Bearer ' + accessToken;
+    }
+    
+    let request = {
+        method: methodRequest,
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: headers,  
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer'    
+    };    
 
-export async function postData(url = '', data = {}, onSuccess, onFailure) {
-    await fetch(url, {
-        method: 'POST',
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'          
-        },  
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *client      
-        body: JSON.stringify(data)
-    })
+    if (hasBody) {
+        request.body = JSON.stringify(data);
+    }
+    
+    await fetch(urlRequest, request)
     .then(res => res.json())
     .then((data) => {       
         onSuccess(data);
     })
     .catch(error => {  
         console.log(error); 
-        onFailure('Сървърна грешка при оторизацията!');
+        onFailure(failureMsg);
     });
+}
+
+export async function postData(url = '', data = {}, onSuccess, onFailure) {
+    baseFetch(url, data, onSuccess, onFailure, 'POST', 'Сървърна грешка при оторизацията!');    
 }
 
 export async function postAuthData(url = '', data = {}, onSuccess, onFailure, methodRequest = 'POST') {  
-    await fetch(url, {
-        method: methodRequest,
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit       
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': 'Bearer ' + accessToken       
-        },        
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *client      
-        body: JSON.stringify(data)
-    })
-    .then(res => res.json())
-    .then((data) => {  
-        onSuccess(data);
-    })
-    .catch(error => {       
-        console.log(error); 
-        onFailure('Сървърна грешка при обновяване на данни!');
-    });
+    baseFetch(url, data, onSuccess, onFailure, methodRequest, 'Сървърна грешка при обновяване на данни!', true, true);
 }
 
 export async function deleteAuthData(url = '', onSuccess, onFailure) {  
-    await fetch(url, {
-        method: 'DELETE',        
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': 'Bearer ' + accessToken       
-        }
-    })
-    .then(res => res.json())
-    .then((data) => {    
-        onSuccess(data);
-    })
-    .catch(error => {  
-        console.log(error);
-        onFailure('Сървърна грешка при изтриване!');
-    });
+    baseFetch(url, {}, onSuccess, onFailure, 'DELETE', 'Сървърна грешка при изтриване!', false, true);
 }
 
 

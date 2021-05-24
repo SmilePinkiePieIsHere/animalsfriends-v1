4cd3afa55b42192948f554216000186aa17181c0
SmilePinkiePieIsHere <pevanzi@abv.bg>
2021-05-24 21:27:57 +0300

    rearranging

--- src/components/Animals/AnimalCard.js
+++ src/components/Animals/AnimalCard.js
@@ -1,7 +1,14 @@
+import { useEffect, useState, Fragment } from "react";
 import { Card, Col, Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { useCookies } from 'react-cookie';
 
+import ModalNotification from "../General/ModalNotification";
+import AlertNotification from "../General/AlertNotification";
+import ThemeContext from "../General/ThemeContext";
+
+import endpoints from "../../services/endpoints.js";
+import { deleteAuthData, getData } from "../../services/services.js";
 import { genderAnimal } from '../General/Helpers/enum.js';
 
 import "./AnimalCard.scss";
@@ -12,9 +19,26 @@ function AnimalCard({
     profileImg,
     gender
 }) {
+    const [popUp, setPopUp] = useState(false);
+    const [alertModal, setAlertModal] = useState({
+        alertShow: false,
+        alertTitle: '',
+        alertText: '',            
+        alertClass: ''
+    });
+    const [shouldUpdate, setShouldUpdate] = useState(false);
     const [cookies] = useCookies(['username']);
     const history = useHistory();
     let isNotLoggedIn = typeof (cookies.username) === "undefined" || cookies.username === "undefined";
+
+    const popUpDelete = (e) => {
+        e.preventDefault();
+        setPopUp(true);
+    }
+
+    const hidePopUp = (e) => {
+        setPopUp(false);
+    }
 
     const goToEdit = (e) => {
         e.preventDefault();
@@ -26,26 +50,61 @@ function AnimalCard({
         history.push(`/animals/details/${id}`);
     }
 
+    const alertDetails = (shouldShow, message, classAlert) => { 
+        setAlertModal({
+            alertShow: shouldShow,
+            alertText: message,            
+            alertClass: classAlert
+        });
+    }
+
+    const deleteAnimal = (e) => {
+        deleteAuthData(`${endpoints.animals}/${id}`, function (data) {
+            setPopUp(false);
+
+            if (!data.error_description) {
+                setShouldUpdate(true);
+
+                alertDetails(true, "Успешно изтрито животно.", "success");
+                setTimeout(() => {
+                    history.push("/animals");
+                }, 2000);
+            }
+            else {
+                alertDetails(true, "Грешка от страна на сървъра при изтриване на животното. Моля опитайте по-късно.", "danger");
+                history.goBack();
+            }
+        }, function (error) {
+            alertDetails(true, "Грешка от страна на сървъра при изтриване на животното. Моля опитайте по-късно.", "danger");
+            history.goBack();
+        });
+    }
+
     return (
-        <Col>
-            <Card className="animal-card">
-                <Card.Body>
-                    <p className="profile-img">
-                        {
-                            profileImg
-                            ? <img alt={name} src={profileImg} />
-                            : <span className="no-image">No image</span>
-                        }
-                    </p>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{genderAnimal[gender]}</Card.Subtitle>
-                    <div className="details">
-                        {!isNotLoggedIn && <Button onClick={goToEdit}>Редактирай</Button>}
-                        <Button onClick={goToDetails}>Детайли</Button>
-                    </div>
-                </Card.Body>
-            </Card>
-        </Col>
+        <>
+            <Col>
+                <Card className="animal-card">
+                    <Card.Body>
+                        <p className="profile-img">
+                            {
+                                profileImg
+                                    ? <img alt={name} src={profileImg} />
+                                    : <span className="no-image">No image</span>
+                            }
+                        </p>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">{genderAnimal[gender]}</Card.Subtitle>
+                        <div className="details">
+                            {!isNotLoggedIn && <Button onClick={popUpDelete}>Изтрий</Button>}
+                            {!isNotLoggedIn && <Button onClick={goToEdit}>Редактирай</Button>}
+                            <Button onClick={goToDetails}>Детайли</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <ModalNotification text="Сигурен ли сте, че искате да премахнете това животно?" variant="warning" show={popUp} onSuccess={deleteAnimal} onCancel={hidePopUp} />
+            <AlertNotification text={alertModal.alertText} heading={alertModal.alertTitle} variant={alertModal.alertClass} show={alertModal.alertShow} />
+        </>
     )
 }
 
--- src/components/Animals/AnimalDetails.js
+++ src/components/Animals/AnimalDetails.js
@@ -1,89 +1,28 @@
-import { Card, Col, Button } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 import { useEffect, useState, Fragment } from "react";
-import { useCookies } from 'react-cookie';
-import { NavLink } from "react-router-dom";
-
-import ModalNotification from "../General/ModalNotification";
-import AlertNotification from "../General/AlertNotification";
-import ThemeContext from "../General/ThemeContext";
 
 import endpoints from "../../services/endpoints.js";
-import { deleteAuthData, getData } from "../../services/services.js";
+import { getData } from "../../services/services.js";
 import { genderAnimal, statusAnimal } from '../General/Helpers/enum.js';
 
 import "./AnimalDetails.scss";
 
 function AnimalDetails({
-    match,
-    history
+    match
 }) {
-    const [animal, setAnimal] = useState({});
-    const [popUp, setPopUp] = useState(false);
-    const [alertModal, setAlertModal] = useState({
-        alertShow: false,
-        alertTitle: '',
-        alertText: '',            
-        alertClass: ''
-    });
-    const [shouldUpdate, setShouldUpdate] = useState(false);
-    const [cookies] = useCookies(['username']);
-
-    let isLoggedIn = !(typeof (cookies.username) === "undefined" || cookies.username === "undefined");
-
+    const [animal, setAnimal] = useState({}); 
+    
     useEffect(() => {
         let animalUrl = `${endpoints.animals}/${match.params.animalId}`;
         getData(animalUrl, function (error) {
             alert(error);
         }, "Грешка от страна на сървъра при вземане на животнo!")
             .then(res => setAnimal(res));
-    }, [alertModal]);
+    }, []); //alertModal
 
-    const goToEdit = (e) => {
-        e.preventDefault();
-        history.push(`/animals/${animal.id}/edit`);
-    }
-
-    const popUpDelete = (e) => {
-        e.preventDefault();
-        setPopUp(true);
-    }
-
-    const hidePopUp = (e) => {
-        setPopUp(false);
-    }
-
-    const alertDetails = (shouldShow, message, classAlert) => { 
-        setAlertModal({
-            alertShow: shouldShow,
-            alertText: message,            
-            alertClass: classAlert
-        });
-    }
-
-    const deleteAnimal = (e) => {
-        deleteAuthData(`${endpoints.animals}/${animal.id}`, function (data) {
-            setPopUp(false);
-
-            if (!data.error_description) {
-                setShouldUpdate(true);
-
-                alertDetails(true, "Успешно изтрито животно.", "success");
-                setTimeout(() => {
-                    history.push("/animals");
-                }, 2000);
-            }
-            else {
-                alertDetails(true, "Грешка от страна на сървъра при изтриване на животното. Моля опитайте по-късно.", "danger");
-                history.goBack();
-            }
-        }, function (error){  
-            alertDetails(true, "Грешка от страна на сървъра при изтриване на животното. Моля опитайте по-късно.", "danger");
-            history.goBack();
-        });
-    }
 
     return (
-        <ThemeContext.Provider value={shouldUpdate}>
+        // <ThemeContext.Provider value={shouldUpdate}>
             <Fragment>
                 <Col>
                     <Card className="wrap-animal">
@@ -98,19 +37,11 @@ function AnimalDetails({
                             <Card.Title>{animal.name}</Card.Title>
                             <Card.Subtitle className="mb-2 text-muted">{genderAnimal[animal.gender]}/{statusAnimal[animal.currentStatus]}</Card.Subtitle>
                             <Card.Text>{animal.description}</Card.Text>
-                            {isLoggedIn ? (
-                                <Card.Text className="details">                                    
-                                    <Button onClick={goToEdit}>Редактирай</Button>
-                                    <Button onClick={popUpDelete}>Изтрий</Button>
-                                </Card.Text>
-                            ) : null}
                         </Card.Body>
                     </Card>
-                </Col>
-                <ModalNotification text="Сигурен ли сте, че искате да премахнете това животно?" variant="warning" show={popUp} onSuccess={deleteAnimal} onCancel={hidePopUp} />
-                <AlertNotification text={alertModal.alertText} heading={alertModal.alertTitle} variant={alertModal.alertClass} show={alertModal.alertShow} />
+                </Col>                
             </Fragment>
-        </ThemeContext.Provider>
+        // </ThemeContext.Provider>
     );
 }
 

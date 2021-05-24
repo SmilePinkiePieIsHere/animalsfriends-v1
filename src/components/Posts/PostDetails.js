d98e8026cd0fcb9d8661ea99a11ce9327f19c530
SmilePinkiePieIsHere <pevanzi@abv.bg>
2021-05-24 18:14:01 +0300

    in edit

--- src/components/Posts/PostDetails.js
+++ src/components/Posts/PostDetails.js
@@ -1,6 +1,8 @@
 import { Card, Col, Container, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
+import AlertNotification from "../General/AlertNotification";
+
 import endpoints from "../../services/endpoints.js";
 import { getData } from "../../services/services.js";
 
@@ -18,27 +20,38 @@ function PostDetails({
         alertClass: ''
     });
 
+    const alertDetails = (shouldShow, message, classAlert) => { 
+        setAlertModal({
+            alertShow: shouldShow,
+            alertText: message,            
+            alertClass: classAlert
+        });
+    }
+
     useEffect(() => {
-        getData(`${endpoints.posts}/${match.params.postId}`, function (error) {
-            alert(error);
+        getData(`${endpoints.posts}/${match.params.postId}`, function (error) {            
+            alertDetails(true, "Грешка от страна на сървъра при вземане на публикацията!", "danger");
         }, "Грешка от страна на сървъра при вземане на публикацията!")
             .then(res => setPost(res));
     }, [alertModal]);
 
     return (
-        <Container>
-            <Row>
-                <Col xs={12}>
-                    <Card className="post-card">
-                        <Card.Header>{post.title}</Card.Header>
-                        <Card.Body>
-                            <Card.Text>
-                                {post.description}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col></Row>
-        </Container>
+        <>
+            <Container>
+                <Row>
+                    <Col xs={12}>
+                        <Card className="post-card">
+                            <Card.Header>{post.title}</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {post.description}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col></Row>
+            </Container>
+            <AlertNotification text={alertModal.alertText} heading={alertModal.alertTitle} variant={alertModal.alertClass} show={alertModal.alertShow} />
+        </>
     )
 }
 
